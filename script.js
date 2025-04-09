bb8f2eef4242cc1faa2dc9044b960e2e29560c98
DalaxDev <daaparradomo@unal.edu.co>
2025-04-09 13:49:08 -0500

    Se agrega interfaz de usuario al juego, adicionalmente se modifica la logica de rondas del juego por lo que para ganar se requieren de 5 victorias

--- script.js
+++ script.js
@@ -1,122 +1,131 @@
-console.log('Hello World')
+   
 
-// console.log(getComputerChoice())
+   const botones = document.querySelectorAll("button")
+   let humanScore = 0;
+   let computerScore = 0;
+   let count = 0;
+   const mensajeRonda = document.getElementById("mensajeRonda");
+   const scoreHumano = document.getElementById("humanoScore");
+   const scoreMaquina = document.getElementById("maquinaScore");
+   const resultadoFinal = document.getElementById("resultadoFinal");
 
-function getComputerChoice(){
-    randomNumber = Math.floor(Math.random()*3)+ 1
-    if (randomNumber === 1){
-        return "PIEDRA";
+  botones.forEach((buttton) => {
+    
+    buttton.addEventListener("click",()=> {
+        // eliminar el resultado final luego de que se reinicia el juego
+        if (count === 0) {
+            resultadoFinal.textContent = "";
+          }
+         // Se obtiene la eleccion del usuario
+        const humanChoice = buttton.id;
 
-    }else if (randomNumber === 2){
-        return "PAPEL";
-    } else {
-        return "TIJERA";
+        // funcion para obtener la opcion de la maquina
+        function getComputerChoice(){
+            randomNumber = Math.floor(Math.random()*3)+ 1
+            if (randomNumber === 1){
+                return "PIEDRA";
+        
+            }else if (randomNumber === 2){
+                return "PAPEL";
+            } else {
+                return "TIJERA";
+            }
+        }; 
+
+          // almacenamiento de la opcion de la maquina
+        const computerChoice = getComputerChoice();
+
+        const mensaje = playRound(humanChoice,computerChoice);
+
+        // contador de rondas jugadas
+        count++;
+
+        function playRound(humanChoice,computerChoice){
+            let mensaje = "";
+            if (computerChoice === "PIEDRA"){
+                switch (humanChoice){
+                    case "PIEDRA" :
+                        mensaje = "Empate, nadie gana punto"; 
+                        break;
+                    case "PAPEL" :
+                        mensaje = "GANASTE !! El papel le gana a la piedra";
+                        humanScore += 1;
+                        break;
+                    case "TIJERA":
+                        mensaje = "PERDISTE !! La piedra gana contra la tijera";
+                        computerScore += 1;
+                        break;
+                     default : 
+                       mensaje =("Debes Ingresar una opcion valida");
+                       computerScore += 1;
+                       
+                }
+            } else if (computerChoice === "PAPEL"){
+                switch (humanChoice){
+                    case "PIEDRA" :
+                        mensaje = "PERDISTE !! El papel le gana a la piedra";
+                        computerScore += 1;
+                        break;
+                    case "PAPEL" :
+                        mensaje = "Empate, nadie gana punto";
+                        break;
+                    case "TIJERA":
+                        mensaje = "GANASTE !! La tijera gana contra el papel";
+                        humanScore += 1;
+                        break;
+                     default : 
+                       mensaje = "Debes Ingresar una opcion valida";
+                       computerScore += 1;
+                }
+            } else if (computerChoice === "TIJERA") {
+                switch (humanChoice){
+                    case "PIEDRA" :
+                        mensaje = "GANASTE !! La piedra gana contra la tijera";
+                        humanScore += 1;
+                        break;
+                    case "PAPEL" :
+                        mensaje = "PERDISTE !! La tijera gana contra el papel";
+                        computerScore += 1;
+                        break;
+                    case "TIJERA":
+                        mensaje = "Empate, nadie gana punto" ;
+                        break;
+                     default : 
+                       mensaje = "Debes Ingresar una opcion valida" ;
+                       computerScore += 1;
+                                }
+                 }
+                 return mensaje
+
+        }
+
+        mensajeRonda.textContent = `Ronda ${count}: Tú elegiste ${humanChoice}, la máquina eligió ${computerChoice}. ${mensaje}`;
+
+        // Actualizar puntajes en pantalla
+           scoreHumano.textContent = humanScore;
+           scoreMaquina.textContent = computerScore;
+
+      // Verificar si alguien llegó a 5 puntos
+       
+       if (humanScore === 5 || computerScore === 5) {
+       if (humanScore > computerScore) {
+        resultadoFinal.textContent = `¡Ganaste el juego! Puntaje final: ${humanScore} - ${computerScore}`;
+      } else {
+        resultadoFinal.textContent = `¡Perdiste el juego! Puntaje final: ${humanScore} - ${computerScore}`;
+      }
+
+      // Reiniciar juego
+      humanScore = 0;
+      computerScore = 0;
+      count = 0;
+      
     }
-}
-// let personChoice = String(window.prompt("Elije piedra, papel o tijera",""));
-function getHumanChoice (){
-    let personChoice = String(window.prompt("Elije piedra, papel o tijera",""));
-    personChoice = personChoice.toUpperCase();
-    return personChoice
-}
 
 
-// console.log(getHumanChoice())
 
-let humanScore = 0;
-let computerScore = 0;
+    });
+    
+    
+  });
 
-function playRound(humanChoice,computerChoice){
-    if (computerChoice === "PIEDRA"){
-        switch (humanChoice){
-            case "PIEDRA" :
-                window.alert("Empate, nadie gana punto");
-                break;
-            case "PAPEL" :
-                window.alert("GANASTE !! El papel le gana a la piedra");
-                humanScore += 1;
-                break;
-            case "TIJERA":
-                window.alert("PERDISTE !! La piedra gana contra la tijera");
-                computerScore += 1;
-                break;
-             default : 
-               window.alert("Debes Ingresar una opcion valida");
-               computerScore += 1;
-               
-        }
-    } else if (computerChoice === "PAPEL"){
-        switch (humanChoice){
-            case "PIEDRA" :
-                window.alert("PERDISTE !! El papel le gana a la piedra")
-                computerScore += 1;
-                break;
-            case "PAPEL" :
-                window.alert("Empate, nadie gana punto");
-                break;
-            case "TIJERA":
-                window.alert("GANASTE !! La tijera gana contra el papel");
-                humanScore += 1;
-                break;
-             default : 
-               window.alert("Debes Ingresar una opcion valida");
-               computerScore += 1;
-        }
-    } else if (computerChoice === "TIJERA") {
-        switch (humanChoice){
-            case "PIEDRA" :
-                window.alert("GANASTE !! La piedra gana contra la tijera")
-                humanScore += 1;
-                break;
-            case "PAPEL" :
-                window.alert("PERDISTE !! La tijera gana contra el papel");
-                computerScore += 1;
-                break;
-            case "TIJERA":
-                window.alert("Empate, nadie gana punto");
-                humanScore += 1;
-                break;
-             default : 
-               window.alert("Debes Ingresar una opcion valida");
-               computerScore += 1;
-                        }
-         }
-         return computerScore,humanScore
-}
 
-// const humanSelection = getHumanChoice()
-// const computerSelection = getComputerChoice()
-
-// playRound(humanSelection, computerSelection);
-
-function playGame () {
-    // let humanScore = 0;
-    // let computerScore = 0;
-    // const humanSelection = getHumanChoice()
-    // const computerSelection = getComputerChoice()
-    let count = 1; 
-
-    while (count <= 5 ) {
-        window.alert("Ronda: " + count)
-        const humanSelection = getHumanChoice();
-        const computerSelection = getComputerChoice();
-        // window.alert("Ronda: " + count) 
-        playRound(humanSelection, computerSelection);
-        console.log("Ronda :" + count);
-        console.log("Humano :" + humanSelection);
-        console.log("Maquina :" + computerSelection);
-        console.log("Score Humano :" + humanScore);
-        console.log("Score computer :" + computerScore);
-        count += 1;
-    } 
-     if (humanScore > computerScore){
-        window.alert ("Humano gana con: "+ humanScore + " Puntos contra: "+computerScore+" de la maquina")
-     } else if (humanScore < computerScore){
-        window.alert ("Humano pierde con: "+ humanScore + " Puntos contra: "+computerScore+" de la maquina")
-     } else {
-        window.alert ("Empate a: " + humanScore + " Puntos")
-     }
-
-}
-
-playGame();
